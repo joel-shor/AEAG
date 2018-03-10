64e656f818c69a03664dfdc7ff881613da703cf9
joel-shor <joelshor@google.com>
2018-03-10 16:30:40 +0100

    Enable translation API check.
    Also, convert assertions to value errors so the debug messages are
    more information.
    
    Tested:
    ./run_tests.sh

--- check_public_apis.py
+++ check_public_apis.py
@@ -23,9 +23,14 @@ from credentials import credentials
 
 def test_get_translate(credentials):
     word_list = ['water']
+    expected_translations = ['מָיִם', 'מים']
     translation_list = translation.get_translations(word_list, credentials, target_language='iw')
-    assert len(translation_list) == len(word_list)
-    assert translation_list[0] == 'מָיִם'
+    if len(translation_list) != len(word_list):
+        raise ValueError(
+            'Expected translation list to be length %i, but was %i.' % (len(word_list), len(translation_list)))
+    if translation_list[0] not in expected_translations:
+        raise ValueError('Expected translation to be one of `%s`, but instead was `%s`.' % (
+            expected_translations, translation_list[0]))
 
 
 def test_get_images(credentials):
@@ -34,7 +39,8 @@ def test_get_images(credentials):
     }
     assert not os.path.exists(filenames_to_write_imgs['water'])
     images.get_images(filenames_to_write_imgs, credentials)
-    assert os.path.exists(filenames_to_write_imgs['water'])
+    if not os.path.exists(filenames_to_write_imgs['water']):
+        raise ValueError('Image file for `water` wasn\'t written.')
 
 
 def test_get_audio(credentials):
@@ -43,10 +49,12 @@ def test_get_audio(credentials):
     }
     assert not os.path.exists(filenames_to_write_imgs['מים'])
     audio.get_audio(filenames_to_write_imgs, credentials)
-    assert os.path.exists(filenames_to_write_imgs['מים'])
+    if not os.path.exists(filenames_to_write_imgs['מים']):
+        raise ValueError('Audio file for `מים` wasn\'t written.')
 
 
 if __name__ == '__main__':
+    test_get_translate(credentials)
     test_get_images(credentials)
     test_get_audio(credentials)
     print('All tests succeeded.')
